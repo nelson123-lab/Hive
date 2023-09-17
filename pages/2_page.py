677c4d6a700182f1ed63670313375b15071d63cb
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-17 00:00:42 -0500

    css_update

--- pages/2_page.py
+++ pages/2_page.py
@@ -1,29 +1,12 @@
 import streamlit as st
 
-st.title("What are you here for ?")
-
 def main():
     # Set the page layout to center the buttons
-    st.markdown(
-        """
-        <style>
-        .center {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    # Create a container div with the center class
-    st.markdown('<div class="center">', unsafe_allow_html=True)
+    with open('style.css') as f:
+            st.markdown('<style>(f.read())</style>', unsafe_allow_html=True)
 
     # Add the donate button
-    if st.button("Donate an Item"):
+    if st.button("Donate an Item", key="donate_button"):
         # Add your donate button logic here
         st.write("Donate button clicked!")
 
@@ -31,7 +14,7 @@ def main():
     st.markdown('<div style="height: 20px;"></div>', unsafe_allow_html=True)
 
     # Add the receive button
-    if st.button("Receive an Item"):
+    if st.button("Receive an Item", key="receive_button"):
         # Add your receive button logic here
         st.write("Receive button clicked!")
 
