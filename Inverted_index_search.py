8112ca1f81bd4483167ea0ed46e8d2995777ea43
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-21 12:26:50 -0500

    working_inverted_index_search

--- Inverted_index_search.py
+++ Inverted_index_search.py
@@ -1,4 +1,20 @@
 from collections import defaultdict
+import pandas as pd
+import os
+from PIL import Image
+import re
+import unicodedata
+
+def basic_clean(string):
+    '''
+    This function takes in a string and
+    returns the string normalized.
+    '''
+    string = unicodedata.normalize('NFKD', string)\
+             .encode('ascii', 'ignore')\
+             .decode('utf-8', 'ignore')
+    string = re.sub(r'[^\w\s]', '', string).lower()
+    return string
 
 class InvertedIndex:
     def __init__(self):
@@ -8,7 +24,6 @@ class InvertedIndex:
         tokens = self._tokenize(description)
         for token in tokens:
             self.index[token].add(product_id)
-        print(tokens)
 
     def search_products(self, query):
         tokens = self._tokenize(query)
@@ -23,20 +38,44 @@ class InvertedIndex:
     def _tokenize(self, text):
         # Implement your tokenization logic here
         # This can include removing stop words, stemming, etc.
+        text = basic_clean(text)
         return text.lower().split()
+
+
+def open_image_based_on_number(image_files, number):
+    for file in image_files:
+        file_name = os.path.splitext(file)[0]
+        if file_name == str(number):
+            image_path = os.path.join(folder_path, file)
+            # Open the image using your preferred method or library
+            image = Image.open(image_path)
+            image.show()
 
 # Example usage
 index = InvertedIndex()
+items_data = pd.read_csv('data/items/info.txt')
 
-# Add products to the index
-index.add_product(1, "Apple iPhone 12 Pro")
-index.add_product(2, "Samsung Galaxy S21 Ultra")
-index.add_product(3, "Apple MacBook Pro")
-index.add_product(4, "Realme 5 Pro")
+for idx, row in items_data.iterrows():
+    index.add_product(int(row['index']), row['items'])
 
-# Search for products
-results = index.search_products("Apple iPhone")
-print(results)  # Output: {1}
+# Search for products 
+results = index.search_products(input())
 
-results = index.search_products("Pro")
-print(results)  # Output: {1, 3}
+"""
+Since we have images of different formats, we won't be able to return images based on the index number since it won't 
+load the images when the format is different.
+Either we need to convert all the images to a single format while uploading them to the database.
+If we are keeping the index the name of the image with the format instead of just the index number.
+"""
+# Example usage
+folder_path = "data/items"
+items = os.listdir(folder_path)
+flt_items = [item for item in items if not item.endswith('.txt')]
+
+if not results:
+    print("No results found")
+elif len(results) == 1:
+    open_image_based_on_number(flt_items, results[0])
+else:
+    for i in results:
+        open_image_based_on_number(flt_items, i)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from PIL import Image
 import pandas as pd
 data = pd.read_csv('data/items/info.txt', index_col='index')
 print(data)
-# image = Image.open('data/items/1.jpg')
-# image.show()
+image = Image.open('data/items/1.jpg')
+image.show()
+
 
